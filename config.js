b87361e38bebbb50444db5dd4715fe8a47a806e5
Alexcitten <61086853+Alexcitten@users.noreply.github.com>
2022-07-17 10:56:16 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     botClientID: "954787535001882625",
-    botPrefix: "s!",
+    botPrefix: "s!", // те, кто знают бота с 2021 - поймут
     ownerID: "397340488438513664",
     token: "bot token"
 }
