5cc5737eda53430d293aa69775b2041c9ce170ba
Alexcitten <61086853+Alexcitten@users.noreply.github.com>
2022-12-24 19:31:23 +0300

    Update help.js

--- slashCommands/Bot/help.js
+++ slashCommands/Bot/help.js
@@ -9,8 +9,9 @@ module.exports = {
         const embed = new EmbedBuilder()
         .setTitle(`Commands`)
         .addFields([
-            {name: `<:wallet:1047085806696808538>  Wallet`, value: `❯ \`/createwallet\` - **Create AR wallet**\n ❯ \`/balance\` - **Check balance**\n ❯ \`/arsend\` - **Send AR**\n ❯ \`/ardrivesend\` - **Send smart-token ArDrive**`, inline: true},
-            {name: `<:dac:1047085787864379392>  Other`, value: `❯ \`/help\` - **List commands**\n ❯ \`/botinfo\` - **About the bot**\n ❯ \`/invite\` - **Useful links**\n ❯ \`/arweave\` - **About Arweave**`, inline: true}
+            {name: `<:wallet:1047085806696808538>  Arweave`, value: `❯ \`/createwallet\` - **Create AR wallet**\n ❯ \`/balance\` - **Check balance**\n ❯ \`/arsend\` - **Send AR**\n ❯ \`/ardrivesend\` - **Send smart-token ArDrive**\n\`/uploaddata\` - **Upload data to main network Arweave**\n \`/information\` - **About Arweave**`, inline: true},
+            {name: `<:beta1:1056241303240839211><:beta2:1056241301605056542>  Handshake`, value: `❯ \`/createwallet\` - **Create HNS wallet**\n ❯ \`/received\` - **Total amount received by specified address**\n ❯ \`/nameinfo\` - **Information on a given name**\n ❯ \`/utxoinfo\` - **Information about UTXO\'s from Chain**\n\`/gettxout\` - ***Get outpoint of the transaction**\n \`/sendhns\` - **Create and template a transaction (useful for multisig). Does not broadcast or add to wallet.**\n\`/validateaddress\` - **Validates address**\n\`/signtx\` - **Sign a templated transaction (useful for multisig)**`, inline: true},
+            {name: `<:dac:1047085787864379392>  Bot`, value: `❯ \`/help\` - **List commands**\n ❯ \`/botinfo\` - **About the bot**\n ❯ \`/invite\` - **Useful links**\n`, inline: true}
         ])
         .setColor('#FF8747')
 
